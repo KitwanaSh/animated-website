017cb4ad9fdbc35696ff47581f9c724fe246934c
KitwanaSh <rodriguekitweze@gmail.com>
2024-12-01 17:09:25 +0200

    Feat: Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Hero from "./components/Hero";
 import NavBar from "./components/Navbar";
 import Features from "./components/Features";
 import Story from "./components/Story";
-// import Contact from "./components/Contact";
-// import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -14,6 +14,8 @@ function App() {
       <About />
       <Features />
       <Story />
+      <Contact />
+      <Footer />
     </main>
   );
 }
